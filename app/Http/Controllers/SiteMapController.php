153e68cd19a522aa2ccb228fc34a221400a9549f
sasasa <masaakisaeki@gmail.com>
2021-01-08 11:33:06 +0900

    sitemap.xmlを作る #21
    画像のパスを追加

--- app/Http/Controllers/SiteMapController.php
+++ app/Http/Controllers/SiteMapController.php
@@ -14,7 +14,18 @@ class SiteMapController extends Controller
         $sitemap = \App::make("sitemap");
         $now = Carbon::now();
         // add items to the sitemap (url, date, priority, freq)
-        $sitemap->add(\URL::to('/'), $now, '1.0', 'weekly');
+
+        $articles = Article::orderBy('id', 'DESC')->limit(4)->get();
+        $images = [];
+        foreach ($articles as $article) {
+            $images[] = [
+                'url' => \URL::to('/storage/'. $article->image_path),
+                'title' => $article->title,
+                'caption' => $article->title,
+                'geo_location' => '',
+            ];
+        }
+        $sitemap->add(\URL::to('/'), $now, '1.0', 'weekly', $images);
         // \URL::to('/home')はweb.phpでトップページに指定したURL　＋ α　をaddメソッドで指定している
 
         $sitemap->add(\URL::to('/search_feature'), $now, '0.9', 'weekly');
@@ -27,6 +38,13 @@ class SiteMapController extends Controller
             
             // add item with images
             $images = [];
+            $images[] = [
+                'url' => \URL::to('/storage/'. $park->image_path),
+                'title' => $park->park_name,
+                'caption' => $park->park_type,
+                'geo_location' => $park->address,
+            ];
+
             foreach ($park->photos as $photo) {
                 $images[] = [
                     'url' => \URL::to('/storage/'. $photo->image_path),
@@ -40,7 +58,14 @@ class SiteMapController extends Controller
 
         $articles = Article::orderBy('created_at', 'DESC')->get();
         foreach ($articles as $article) {
-            $sitemap->add(\URL::to('/article/'. $article->id), $now, '0.9', 'weekly');
+            $images = [];
+            $images[] = [
+                'url' => \URL::to('/storage/'. $article->image_path),
+                'title' => $article->title,
+                'caption' => $article->title,
+                'geo_location' => '',
+            ];
+            $sitemap->add(\URL::to('/article/'. $article->id), $now, '0.9', 'weekly', $images);
         }
 
         
