94ed3344de48d1562ecdb9deb230ff5ae4b91d1e
masasaeki <masaakisaeki@gmail.com>
2021-01-22 14:35:26 +0900

    利用規約のダミー

--- resources/views/root/terms_of_use.blade.php
+++ resources/views/root/terms_of_use.blade.php
@@ -5,6 +5,37 @@
 @section('title', '利用規約')
 
 @section('content')
-<h1>利用規約</h1>
-<p>あれこれあれこれあれこれあれこれ</p>
+<section id="section_privacy_area">
+  <h2>利用規約</h2>
+    <div id="praivacy_area">
+        <article class="privacy_outer">
+            <h3>利用規約</h3>
+            <p>
+              利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+              利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+              利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約</p>
+            <article class="privacy_inner">
+                <h4>1)利用規約</h4>
+                <p>
+                  利用規約利用規約 利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+                  利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+                </p>
+            </article>
+            <article class="privacy_inner">
+                <h4>2)利用規約</h4>
+                <p>
+                  利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+                  利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+                  利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約利用規約
+                </p>
+            </article>
+        </article><!-- class="privacy_outer" -->
+
+        <div id="preparer">
+          <p>令和3年1月1日&nbsp;策定
+            <span>株式会社Grow-up&nbsp;わくわくLab</span>
+          </p><!-- id="preparer" -->
+        </div>
+    </div><!-- id="praivacy_area" -->
+</section><!-- id="section_privacy_area" -->
 @endsection
